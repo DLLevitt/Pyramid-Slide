235edc0cc74a6991e4e38638da0d68888026a7a8
Levitt <123530523+DLLevitt@users.noreply.github.com>
2023-03-01 23:37:09 +0000

    $ git commit -m "render pyramid to the DOM instead of the console"

--- mario.js
+++ mario.js
@@ -5,8 +5,20 @@ function printPyramid(height, currentStep = 2) {
   if (height === 0) {
     return;
   }
-  const spaces = " ".repeat(height - 1);
-  console.log(spaces + "#".repeat(currentStep));
+  const spaces = ".".repeat(height - 1);
+  let rowStr = spaces + "#".repeat(currentStep);
+  console.log(rowStr);
+  
+  
+  const pyramidDiv = document.getElementById("pyramid");
+  // const rowStr = printPyramid(5);
+  let para = document.createElement("p");
+  para.innerHTML = rowStr;
+  // para.appendChild(rowStr);
+
+  //last step
+  pyramidDiv.appendChild(para);
   printPyramid(height - 1, currentStep + 1);
 }
+
 
